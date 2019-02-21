11f9c8f1ae3c607e68a3be8f207a56f629015d8f
utopia-developer <utopia0developer@gmail.com>
2019-02-21 14:56:22 +0800

    Java SE chapter1 Java初始

---
+++ BubbleSort.java
@@ -0,0 +1,18 @@
+public class BubbleSort{
+    public static void main(String[] args){
+        int[] sort = {34, 53, 12, 32, 56, 17};
+        int temp;
+        for(int i = 0; i < sort.length - 1; i++){
+            for(int j = 0; j < sort.length - 1 - i; j++){
+                if(sort[j] > sort[j + 1]){
+                    temp = sort[j];
+                    sort[j] = sort[j + 1];
+                    sort[j + 1] = temp;
+                }
+            }
+        }
+        for(int n : sort){
+            System.out.print(n + "  ");
+        }
+    }
+}
---
+++ chapter1/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * HelloWorld
+ * 第一个Java程序，了解Java代码结构及执行流程
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
