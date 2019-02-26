ecf3e5659187158fa3cf6b42ca01756521966e80
utopia-developer <utopia0developer@gmail.com>
2019-02-26 14:34:58 +0800

    chapter5 Java流程控制之循环结构 完

--- chapter5_流程控制之循环结构/Quiz5_3.java
+++ chapter5_流程控制之循环结构/Quiz5_3.java
@@ -6,7 +6,13 @@ public class Quiz5_3 {
 
     public static void main(String[] args) {
         for(int i = 1; i <= 5;i ++){
-            for(int j = )
+            for(int j = 5 - i; j >= 0; j--){
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= (2 * i + 1); k++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
