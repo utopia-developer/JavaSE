6a38e04a0ae18edac27ecf23e53e5a952f63aa12
utopia-developer <utopia0developer@gmail.com>
2019-02-27 13:10:35 +0800

    chapter6 Java数组之一维数组 完

---
+++ chapter6_数组之一维数组/Quiz2_6.java
@@ -0,0 +1,17 @@
+/**
+ * Quiz2_6
+ */
+public class Quiz2_6 {
+
+    public static void main(String[] args) {
+        // 定义一个整型数组，并初始化
+        int[] array = { 1, 2, 6, 12, 15, 16, 19 };
+        // 循环遍历数组，找出能被3整除的元素并打印输出
+        System.out.println("能被3整除的数组元素为：");
+        for (int n : array) {
+            if (n % 3 == 0) {
+                System.out.println(n);
+            }
+        }
+    }
+}
---
+++ chapter6_数组之一维数组/Quiz4_4.java
@@ -0,0 +1,16 @@
+/**
+ * Quiz4_4
+ */
+public class Quiz4_4 {
+
+    public static void main(String[] args) {
+        char[] array = { 'a', 'A', 'c', '1', 'a', '2', '3', 'A' };
+        int amount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 'a' || array[i] == 'A') {
+                amount++;
+            }
+        }
+        System.out.println("字符数组中字符'a'或'A'的个数为：" + amount);
+    }
+}
