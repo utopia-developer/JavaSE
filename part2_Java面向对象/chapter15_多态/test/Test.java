7e3bc9736f575b391ad44e789f6f49aa36f2bd03
utopia-developer <utopia0developer@gmail.com>
2019-03-10 23:04:57 +1100

    chapter15 Java多态 part2 向下转型

--- part2_Java面向对象/chapter15_多态/test/Test.java
+++ part2_Java面向对象/chapter15_多态/test/Test.java
@@ -6,11 +6,22 @@ import part2_Java面向对象.chapter15_多态.polymorphism.Dog;
 
 public class Test {
     public static void main(String[] args) {
+        //向上转型
         Animal one = new Animal();
         one.eat();
         Animal two = new Cat();
         two.eat();
         Animal three = new Dog();
         three.eat();
+        System.out.println("=============================");
+        //向下转型
+        Cat cat = (Cat) two;
+        cat.eat();
+        cat.run();
+        cat.setName("花花");
+        System.out.println(cat.getName());
+        //ClassCastException - 无法对Cat类型的实例two强制转型为Dog类型
+        Dog dog = (Dog) two;
+        dog.eat();
     }
 }
